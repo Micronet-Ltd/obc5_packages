01a7212477fe0aa1829cc65c9688efc384d8362d
Brigham Diaz <brigham.diaz@micronet-inc.com>
2016-06-08 13:12:12 -0600

    Added exception handling for every API call

--- micronet/mctl-android-example/app/src/main/java/com/micronet/mcontrol/MControl.java
+++ micronet/mctl-android-example/app/src/main/java/com/micronet/mcontrol/MControl.java
@@ -25,7 +25,7 @@ public class MControl {
     private native static int jniGetPowerOnReason();
     private native static int jniSetDevicePowerOff();
     private native static String jniGetRTCDateTime();
-    private native static int jniSetRTCDateTime(long time);
+    private native static void jniSetRTCDateTime(long time);
     private native static int[] jniGetRTCCalReg();
     private native static int jniSetRTCCalReg();
     private native static int jniGetRTCRegDBG();
@@ -39,16 +39,20 @@ public class MControl {
 
     public String get_fpga_version() {
         if(DBG) return "1234DBG";
-        return Integer.toHexString(jniGetFPGAVersion());
-    }
-
-    public void set_led_status(int led, int rgb) {
-        jniSetLEDValue (led, rgb);
+        try {
+            return Integer.toHexString(jniGetFPGAVersion());
+        } catch(Exception e) {
+            return "error=" + e.getMessage();
+        }
     }
 
     public int get_adc_or_gpi_voltage(int gpi_num) {
         if(DBG) return 1234;
-        return jniGetADCorGPIVoltage(gpi_num);
+        try {
+            return jniGetADCorGPIVoltage(gpi_num);
+        } catch(Exception e) {
+            return -1;
+        }
     }
 
     public String get_rtc_date_time() {
@@ -57,7 +61,12 @@ public class MControl {
             Date today = Calendar.getInstance().getTime();
             return formatter.format(today);
         }
-        return jniGetRTCDateTime();
+
+        try {
+            return jniGetRTCDateTime();
+        } catch(Exception e) {
+            return "error=" + e.getMessage();
+        }
     }
 
     /**
@@ -66,16 +75,18 @@ public class MControl {
      * A value of -1 indicates that value is invalid.
      */
     public int[] get_rtc_cal_reg() {
-        if(DBG) {
-            return new int[] {-1,-1};
-        }
+        int[] arr = {-1, -1};
 
-        int[] arr = jniGetRTCCalReg();
-        if(arr.length == 1) {
-            arr = new int[] {arr[0], -1};
-        } else if(arr.length != 2) {
-            arr = new int[] {-1, -1};
-        }
+        if (DBG) { return arr; }
+
+        try {
+            arr = jniGetRTCCalReg();
+            if (arr.length == 1) {
+                arr = new int[]{arr[0], -1};
+            } else if (arr.length != 2) {
+                arr = new int[]{-1, -1};
+            }
+        } catch (Exception e) { }
 
         return arr;
     }
@@ -95,13 +106,53 @@ public class MControl {
         if(DBG) {
             return led;
         }
-
-        int[] arr = jniGetLEDStatus(led_num);
-        led.RED = arr[0];
-        led.GREEN = arr[1];
-        led.BLUE = arr[2];
-        led.BRIGHTNESS = arr[3];
+        try {
+            int[] arr = jniGetLEDStatus(led_num);
+            led.RED = arr[0];
+            led.GREEN = arr[1];
+            led.BLUE = arr[2];
+            led.BRIGHTNESS = arr[3];
+        } catch (Exception e) { }
 
         return led;
     }
+
+    /**
+     * Throws exception
+     *
+     * @param led
+     * @param rgb
+     */
+    public void set_led_status(int led, int rgb) {
+        try {
+            jniSetLEDValue(led, rgb);
+        } catch (Exception e) { throw e; }
+    }
+
+    public void get_power_on_threshold_cfg() { }
+    public void get_power_on_reason() { }
+    public void get_rtc_reg_dbg() { }
+    public void set_power_on_threshold_cfg() {};
+    public void set_device_power_off() {};
+    public void set_rtc_date_time() {};
+    public void set_rtc_cal_reg() {};
+    public void set_rtc_reg_dbg() {};
+
+//    int get_mcu_version(int * fd, uint8_t * fw_version, size_t size);
+//    int get_fpga_version(int * fd, uint32_t * fpga_version, size_t size);
+//    int get_adc_or_gpi_voltage(int * fd, uint8_t gpi_num, uint32_t * gpi_voltage, size_t size);
+//    int get_led_status(int * fd, uint8_t led_num, uint8_t *brightness, uint8_t *red, uint8_t *green, uint8_t *blue);
+//    int set_led_status(int * fd, uint8_t led_num, uint8_t brightness, uint8_t red, uint8_t green, uint8_t blue);
+//    int get_power_on_threshold_cfg(int * fd, uint16_t *wiggle_count, uint16_t *wig_cnt_sample_period, uint16_t *ignition_threshold);
+//    int set_power_on_threshold_cfg(int * fd, uint16_t wiggle_count, uint16_t wig_cnt_sample_period, uint16_t ignition_threshold);
+//    int get_power_on_reason(int * fd, uint8_t *power_on_reason);
+//    int set_device_power_off(int * fd, uint8_t wait_time);
+//    int get_rtc_date_time(int * fd, char * dt_str);
+//    int set_rtc_date_time(int * fd, char * dt_str);
+//    int get_rtc_cal_reg(int * fd, uint8_t * dig_cal, uint8_t * anal_cal);
+//    int set_rtc_cal_reg(int * fd, uint8_t dig_cal, uint8_t analog_cal);
+//    int get_rtc_reg_dbg(int * fd, uint8_t address, uint8_t * data);
+//    int set_rtc_reg_dbg(int * fd, uint8_t address, uint8_t data);
+//    bool check_rtc_battery(int * fd);
+
 }
--- micronet/mctl-android-example/app/src/main/jni/MControl.cpp
+++ micronet/mctl-android-example/app/src/main/jni/MControl.cpp
@@ -1,3 +1,6 @@
+/**
+ * Created by brigham.diaz on 5/24/2016.
+ */
 #include <jni.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -17,6 +20,7 @@ enum LED {
 };
 
 static pthread_mutex_t mutexlock;
+void throwException(JNIEnv *, int);
 
 JNIEXPORT jstring JNICALL
 Java_com_micronet_mcontrol_MControl_jniGetMCUVersion(JNIEnv *env, jclass type) {
@@ -24,13 +28,23 @@ Java_com_micronet_mcontrol_MControl_jniGetMCUVersion(JNIEnv *env, jclass type) {
     int result = 0;
     jstring jresult = NULL;
 
+
     int fd = iosocket_connect();
     if (fd != 0) {
         result = get_mcu_version(&fd, data, 4);
-        snprintf((char *)data, sizeof(data), "%X.%X.%X.%X", data[0], data[1], data[2], data[3]);
         iosocket_disconnect(&fd);
+        if(result != 0) {
+            char err[256];
+            sprintf(err, "%d", result);
+            jclass Exception = env->FindClass("java/lang/Exception");
+            env->ThrowNew(Exception, err);
+        }
+        snprintf((char *)data, sizeof(data), "%X.%X.%X.%X", data[0], data[1], data[2], data[3]);
         jresult = env->NewStringUTF((char *)data);
         return jresult;
+    } else {
+        jclass Exception = env->FindClass("java/lang/Exception");
+        env->ThrowNew(Exception, "iosocket connect failed.");
     }
 
     return jresult;
@@ -40,16 +54,17 @@ JNIEXPORT jint JNICALL
 Java_com_micronet_mcontrol_MControl_jniGetFPGAVersion(JNIEnv *env, jclass type) {
 
     uint32_t fpga_ver = 0;
-    int result = 0;
 
     int fd = iosocket_connect();
     if (fd != 0) {
-        result = get_fpga_version(&fd, &fpga_ver, 4);
-        //LOGI("result: %d, FPGA Version: %X", result, fpga_ver);
-
+        int result = get_fpga_version(&fd, &fpga_ver, 4);
         iosocket_disconnect(&fd);
+        if(result != 0) {
+            throwException(env, result);
+        }
 
-        return fpga_ver;
+    } else {
+        throwException(env, fd);
     }
 
     return fpga_ver;
@@ -59,14 +74,16 @@ Java_com_micronet_mcontrol_MControl_jniGetFPGAVersion(JNIEnv *env, jclass type) 
 JNIEXPORT jint JNICALL
 Java_com_micronet_mcontrol_MControl_jniGetADCorGPIVoltage(JNIEnv *env, jclass type, jint gpi_num) {
     uint32_t voltage = 0;
-    int result = 0;
 
     int fd = iosocket_connect();
     if (fd != 0) {
-        result = get_adc_or_gpi_voltage(&fd, gpi_num, &voltage, sizeof(voltage));
-        //LOGI("result: %d, FPGA Version: %X", result, fpga_ver);
-
+        int result = get_adc_or_gpi_voltage(&fd, gpi_num, &voltage, sizeof(voltage));
         iosocket_disconnect(&fd);
+        if(result != 0) {
+            throwException(env, result);
+        }
+    } else {
+        throwException(env, fd);
     }
 
     return voltage;
@@ -77,20 +94,24 @@ Java_com_micronet_mcontrol_MControl_jniGetLEDStatus(JNIEnv *env, jobject instanc
     int size = 4;
     jintArray newArray = env->NewIntArray(size);
 
-    int result = 0;
-
     int fd = iosocket_connect();
     if (fd != 0) {
         jint tmp[3];
         uint8_t brightness, red, green, blue;
-        result = get_led_status(&fd, led_num, &brightness, &red, &green, &blue);
+        int result = get_led_status(&fd, led_num, &brightness, &red, &green, &blue);
         iosocket_disconnect(&fd);
+        if(result != 0) {
+            throwException(env, result);
+        }
+
         tmp[0] = red;
         tmp[1] = green;
         tmp[2] = blue;
         tmp[3] = brightness;
         // copy jint[] to jintarray
         env->SetIntArrayRegion(newArray, 0, size, tmp);
+    } else {
+        throwException(env, fd);
     }
 
     return newArray;
@@ -98,9 +119,6 @@ Java_com_micronet_mcontrol_MControl_jniGetLEDStatus(JNIEnv *env, jobject instanc
 
 JNIEXPORT void JNICALL
 Java_com_micronet_mcontrol_MControl_jniSetLEDValue(JNIEnv *env, jclass type, jint led, jint rgb) {
-
-    int result = 0;
-
     uint8_t brightness = 0xFF;
     uint8_t red = (rgb & 0xFF0000) >> 16;
     uint8_t green = (rgb & 0x00FF00) >> 8;
@@ -109,11 +127,15 @@ Java_com_micronet_mcontrol_MControl_jniSetLEDValue(JNIEnv *env, jclass type, jin
     pthread_mutex_lock(&mutexlock);
     int fd = iosocket_connect();
     if (fd != 0) {
-        result = set_led_status(&fd, led, brightness, red, green, blue);
-        //LOGI("LED: %d, red: 0x%02X, green: 0x%02X, blue: 0x%02X, result: %d", led, red, green, blue, result);
-
+        int result = set_led_status(&fd, led, brightness, red, green, blue);
         iosocket_disconnect(&fd);
+        if(result != 0) {
+            throwException(env, result);
+        }
+    } else {
+        throwException(env, fd);
     }
+
     pthread_mutex_unlock(&mutexlock);
 
 }
@@ -149,15 +171,19 @@ Java_com_micronet_mcontrol_MControl_jniSetDevicePowerOff(JNIEnv *env, jobject in
 JNIEXPORT jstring JNICALL
 Java_com_micronet_mcontrol_MControl_jniGetRTCDateTime(JNIEnv *env, jobject instance) {
     char dt_str[RTC_STRING_SIZE] = "2016-03-29 19:09:06.58";
-    int result = 0;
     jstring jresult = NULL;
 
     int fd = iosocket_connect();
 
     if (fd != 0) {
-        result = get_rtc_date_time(&fd, dt_str);
+        int result = get_rtc_date_time(&fd, dt_str);
         iosocket_disconnect(&fd);
+        if(result != 0) {
+            throwException(env, result);
+        }
         jresult = env->NewStringUTF(dt_str);
+    } else {
+        throwException(env, fd);
     }
 
     return jresult;
@@ -166,43 +192,51 @@ Java_com_micronet_mcontrol_MControl_jniGetRTCDateTime(JNIEnv *env, jobject insta
 /**
  *  Expected dt_str format: year-month-day hour:min:sec.deciseconds
  * 					  Ex : 2016-03-29 19:09:06.58
- * 	Returns result, 0 is bad
  */
-JNIEXPORT jint JNICALL
+JNIEXPORT void JNICALL
 Java_com_micronet_mcontrol_MControl_jniSetRTCDateTime(JNIEnv *env, jobject instance, jstring time) {
-
     char * dt_str = (char *)env->GetStringUTFChars(time, JNI_FALSE);
 
-    int result = 0;
     int fd = iosocket_connect();
 
     if (fd != 0) {
-        result = set_rtc_date_time(&fd, dt_str);
+        int result = set_rtc_date_time(&fd, dt_str);
         iosocket_disconnect(&fd);
+        if(result != 0) {
+            throwException(env, result);
+        }
+    } else {
+        throwException(env, fd);
     }
-
-    return result;
 }
 
 JNIEXPORT jintArray JNICALL
 Java_com_micronet_mcontrol_MControl_jniGetRTCCalReg(JNIEnv *env, jobject instance) {
     uint8_t rtc_dig_cal, rtc_analog_cal;
     jintArray jarr = env->NewIntArray(2);
-    jint *narr = env->GetIntArrayElements(jarr, NULL);
 
-    int result = 0;
     int fd = iosocket_connect();
 
     if (fd != 0) {
-        result = get_rtc_cal_reg(&fd, &rtc_dig_cal, &rtc_analog_cal);
+        int result = get_rtc_cal_reg(&fd, &rtc_dig_cal, &rtc_analog_cal);
         printf("get rtc cal registers, dig cal: %x analog cal: %x, ret = %d  \n", \
 					rtc_dig_cal, rtc_analog_cal, result);
         iosocket_disconnect(&fd);
+
+        if(result != 0) {
+            // prevent memory leak
+            env->DeleteLocalRef(jarr);
+            throwException(env, result);
+        }
+
+        jint *narr = env->GetIntArrayElements(jarr, NULL);
         narr[0] = rtc_dig_cal;
         narr[1] = rtc_analog_cal;
+        env->ReleaseIntArrayElements(jarr, narr, NULL);
+    } else {
+        throwException(env, fd);
     }
 
-    env->ReleaseIntArrayElements(jarr, narr, NULL);
     return jarr;
 }
 
@@ -228,21 +262,43 @@ Java_com_micronet_mcontrol_MControl_jniSetRTCRegDBG(JNIEnv *env, jobject instanc
 }
 
 /**
- * returns false if RTC battery is bad or register could not be read.
+ * returns false if RTC battery is bad or not present
  * returns true if RTC battery is good.
  */
 JNIEXPORT jboolean JNICALL
 Java_com_micronet_mcontrol_MControl_jniCheckRTCBattery(JNIEnv *env, jobject instance) {
-    int result = 0;
+
     int fd = iosocket_connect();
+    int result = 0;
 
     if (fd != 0) {
-        result = check_rtc_battery(&fd);
+        int result = check_rtc_battery(&fd);
         iosocket_disconnect(&fd);
+        if (result < 0) {
+            throwException(env, result);
+        }
+    } else {
+        throwException(env, fd);
     }
 
-    // 0 indicates no response or bad RTC battery
-    return result != 0;
+    // false = RTC battery is bad or not present
+    // true = RTC battery is good
+    return result == 1;
+}
+
+/**
+ * helper function to throw exception
+ */
+void throwException(JNIEnv *env, int result) {
+    jboolean flag = env->ExceptionCheck();
+    if (flag) {
+        env->ExceptionClear();
+    }
+
+    char err[256];
+    sprintf(err, "%d", result);
+    jclass jcls = env->FindClass("java/lang/jcls");
+    env->ThrowNew(jcls, err);
 }
 
 #ifdef __cplusplus

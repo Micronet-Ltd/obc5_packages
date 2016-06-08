bde4dc582f12238d5a73640794d4e96b58d1b647
Brigham Diaz <brigham.diaz@micronet-inc.com>
2016-06-08 17:33:02 -0600

    removed exceptions

--- micronet/mctl-android-example/app/src/main/jni/MControl.cpp
+++ micronet/mctl-android-example/app/src/main/jni/MControl.cpp
@@ -1,6 +1,3 @@
-/**
- * Created by brigham.diaz on 5/24/2016.
- */
 #include <jni.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -20,7 +17,6 @@ enum LED {
 };
 
 static pthread_mutex_t mutexlock;
-void throwException(JNIEnv *, int);
 
 JNIEXPORT jstring JNICALL
 Java_com_micronet_mcontrol_MControl_jniGetMCUVersion(JNIEnv *env, jclass type) {
@@ -28,23 +24,13 @@ Java_com_micronet_mcontrol_MControl_jniGetMCUVersion(JNIEnv *env, jclass type) {
     int result = 0;
     jstring jresult = NULL;
 
-
     int fd = iosocket_connect();
     if (fd != 0) {
         result = get_mcu_version(&fd, data, 4);
-        iosocket_disconnect(&fd);
-        if(result != 0) {
-            char err[256];
-            sprintf(err, "%d", result);
-            jclass Exception = env->FindClass("java/lang/Exception");
-            env->ThrowNew(Exception, err);
-        }
         snprintf((char *)data, sizeof(data), "%X.%X.%X.%X", data[0], data[1], data[2], data[3]);
+        iosocket_disconnect(&fd);
         jresult = env->NewStringUTF((char *)data);
         return jresult;
-    } else {
-        jclass Exception = env->FindClass("java/lang/Exception");
-        env->ThrowNew(Exception, "iosocket connect failed.");
     }
 
     return jresult;
@@ -54,17 +40,16 @@ JNIEXPORT jint JNICALL
 Java_com_micronet_mcontrol_MControl_jniGetFPGAVersion(JNIEnv *env, jclass type) {
 
     uint32_t fpga_ver = 0;
+    int result = 0;
 
     int fd = iosocket_connect();
     if (fd != 0) {
-        int result = get_fpga_version(&fd, &fpga_ver, 4);
-        iosocket_disconnect(&fd);
-        if(result != 0) {
-            throwException(env, result);
-        }
+        result = get_fpga_version(&fd, &fpga_ver, 4);
+        //LOGI("result: %d, FPGA Version: %X", result, fpga_ver);
 
-    } else {
-        throwException(env, fd);
+        iosocket_disconnect(&fd);
+
+        return fpga_ver;
     }
 
     return fpga_ver;
@@ -74,16 +59,14 @@ Java_com_micronet_mcontrol_MControl_jniGetFPGAVersion(JNIEnv *env, jclass type) 
 JNIEXPORT jint JNICALL
 Java_com_micronet_mcontrol_MControl_jniGetADCorGPIVoltage(JNIEnv *env, jclass type, jint gpi_num) {
     uint32_t voltage = 0;
+    int result = 0;
 
     int fd = iosocket_connect();
     if (fd != 0) {
-        int result = get_adc_or_gpi_voltage(&fd, gpi_num, &voltage, sizeof(voltage));
+        result = get_adc_or_gpi_voltage(&fd, gpi_num, &voltage, sizeof(voltage));
+        //LOGI("result: %d, FPGA Version: %X", result, fpga_ver);
+
         iosocket_disconnect(&fd);
-        if(result != 0) {
-            throwException(env, result);
-        }
-    } else {
-        throwException(env, fd);
     }
 
     return voltage;
@@ -94,24 +77,20 @@ Java_com_micronet_mcontrol_MControl_jniGetLEDStatus(JNIEnv *env, jobject instanc
     int size = 4;
     jintArray newArray = env->NewIntArray(size);
 
+    int result = 0;
+
     int fd = iosocket_connect();
     if (fd != 0) {
         jint tmp[3];
         uint8_t brightness, red, green, blue;
-        int result = get_led_status(&fd, led_num, &brightness, &red, &green, &blue);
+        result = get_led_status(&fd, led_num, &brightness, &red, &green, &blue);
         iosocket_disconnect(&fd);
-        if(result != 0) {
-            throwException(env, result);
-        }
-
         tmp[0] = red;
         tmp[1] = green;
         tmp[2] = blue;
         tmp[3] = brightness;
         // copy jint[] to jintarray
         env->SetIntArrayRegion(newArray, 0, size, tmp);
-    } else {
-        throwException(env, fd);
     }
 
     return newArray;
@@ -119,6 +98,9 @@ Java_com_micronet_mcontrol_MControl_jniGetLEDStatus(JNIEnv *env, jobject instanc
 
 JNIEXPORT void JNICALL
 Java_com_micronet_mcontrol_MControl_jniSetLEDValue(JNIEnv *env, jclass type, jint led, jint rgb) {
+
+    int result = 0;
+
     uint8_t brightness = 0xFF;
     uint8_t red = (rgb & 0xFF0000) >> 16;
     uint8_t green = (rgb & 0x00FF00) >> 8;
@@ -127,15 +109,11 @@ Java_com_micronet_mcontrol_MControl_jniSetLEDValue(JNIEnv *env, jclass type, jin
     pthread_mutex_lock(&mutexlock);
     int fd = iosocket_connect();
     if (fd != 0) {
-        int result = set_led_status(&fd, led, brightness, red, green, blue);
-        iosocket_disconnect(&fd);
-        if(result != 0) {
-            throwException(env, result);
-        }
-    } else {
-        throwException(env, fd);
-    }
+        result = set_led_status(&fd, led, brightness, red, green, blue);
+        //LOGI("LED: %d, red: 0x%02X, green: 0x%02X, blue: 0x%02X, result: %d", led, red, green, blue, result);
 
+        iosocket_disconnect(&fd);
+    }
     pthread_mutex_unlock(&mutexlock);
 
 }
@@ -171,19 +149,15 @@ Java_com_micronet_mcontrol_MControl_jniSetDevicePowerOff(JNIEnv *env, jobject in
 JNIEXPORT jstring JNICALL
 Java_com_micronet_mcontrol_MControl_jniGetRTCDateTime(JNIEnv *env, jobject instance) {
     char dt_str[RTC_STRING_SIZE] = "2016-03-29 19:09:06.58";
+    int result = 0;
     jstring jresult = NULL;
 
     int fd = iosocket_connect();
 
     if (fd != 0) {
-        int result = get_rtc_date_time(&fd, dt_str);
+        result = get_rtc_date_time(&fd, dt_str);
         iosocket_disconnect(&fd);
-        if(result != 0) {
-            throwException(env, result);
-        }
         jresult = env->NewStringUTF(dt_str);
-    } else {
-        throwException(env, fd);
     }
 
     return jresult;
@@ -192,51 +166,43 @@ Java_com_micronet_mcontrol_MControl_jniGetRTCDateTime(JNIEnv *env, jobject insta
 /**
  *  Expected dt_str format: year-month-day hour:min:sec.deciseconds
  * 					  Ex : 2016-03-29 19:09:06.58
+ * 	Returns result, 0 is bad
  */
-JNIEXPORT void JNICALL
+JNIEXPORT jint JNICALL
 Java_com_micronet_mcontrol_MControl_jniSetRTCDateTime(JNIEnv *env, jobject instance, jstring time) {
+
     char * dt_str = (char *)env->GetStringUTFChars(time, JNI_FALSE);
 
+    int result = 0;
     int fd = iosocket_connect();
 
     if (fd != 0) {
-        int result = set_rtc_date_time(&fd, dt_str);
+        result = set_rtc_date_time(&fd, dt_str);
         iosocket_disconnect(&fd);
-        if(result != 0) {
-            throwException(env, result);
-        }
-    } else {
-        throwException(env, fd);
     }
+
+    return result;
 }
 
 JNIEXPORT jintArray JNICALL
 Java_com_micronet_mcontrol_MControl_jniGetRTCCalReg(JNIEnv *env, jobject instance) {
     uint8_t rtc_dig_cal, rtc_analog_cal;
     jintArray jarr = env->NewIntArray(2);
+    jint *narr = env->GetIntArrayElements(jarr, NULL);
 
+    int result = 0;
     int fd = iosocket_connect();
 
     if (fd != 0) {
-        int result = get_rtc_cal_reg(&fd, &rtc_dig_cal, &rtc_analog_cal);
+        result = get_rtc_cal_reg(&fd, &rtc_dig_cal, &rtc_analog_cal);
         printf("get rtc cal registers, dig cal: %x analog cal: %x, ret = %d  \n", \
 					rtc_dig_cal, rtc_analog_cal, result);
         iosocket_disconnect(&fd);
-
-        if(result != 0) {
-            // prevent memory leak
-            env->DeleteLocalRef(jarr);
-            throwException(env, result);
-        }
-
-        jint *narr = env->GetIntArrayElements(jarr, NULL);
         narr[0] = rtc_dig_cal;
         narr[1] = rtc_analog_cal;
-        env->ReleaseIntArrayElements(jarr, narr, NULL);
-    } else {
-        throwException(env, fd);
     }
 
+    env->ReleaseIntArrayElements(jarr, narr, NULL);
     return jarr;
 }
 
@@ -262,43 +228,21 @@ Java_com_micronet_mcontrol_MControl_jniSetRTCRegDBG(JNIEnv *env, jobject instanc
 }
 
 /**
- * returns false if RTC battery is bad or not present
+ * returns false if RTC battery is bad or register could not be read.
  * returns true if RTC battery is good.
  */
 JNIEXPORT jboolean JNICALL
 Java_com_micronet_mcontrol_MControl_jniCheckRTCBattery(JNIEnv *env, jobject instance) {
-
-    int fd = iosocket_connect();
     int result = 0;
+    int fd = iosocket_connect();
 
     if (fd != 0) {
-        int result = check_rtc_battery(&fd);
+        result = check_rtc_battery(&fd);
         iosocket_disconnect(&fd);
-        if (result < 0) {
-            throwException(env, result);
-        }
-    } else {
-        throwException(env, fd);
     }
 
-    // false = RTC battery is bad or not present
-    // true = RTC battery is good
-    return result == 1;
-}
-
-/**
- * helper function to throw exception
- */
-void throwException(JNIEnv *env, int result) {
-    jboolean flag = env->ExceptionCheck();
-    if (flag) {
-        env->ExceptionClear();
-    }
-
-    char err[256];
-    sprintf(err, "%d", result);
-    jclass jcls = env->FindClass("java/lang/jcls");
-    env->ThrowNew(jcls, err);
+    // 0 indicates no response or bad RTC battery
+    return result != 0;
 }
 
 #ifdef __cplusplus

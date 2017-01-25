646c30108bb8f9b5f82d7634a850113eebcf017e
igorl <igor.lantsman@micronet-inc.com>
2017-01-25 09:08:46 +0200

    Added ignition WakeLock.

--- micronet/suspend_service/Android.mk
+++ micronet/suspend_service/Android.mk
@@ -1,12 +1,39 @@
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES:= suspend_service.c
-LOCAL_MODULE := suspend_service
+#LOCAL_SRC_FILES := AWakeLock.cpp
+#LOCAL_MODULE := libspsrvs
 LOCAL_MODULE_TAGS := optional
-LOCAL_CFLAGS += -std=c99 -DUSE_THREADS -D__ANDROID__
-LOCAL_FORCE_STATIC_EXECUTABLE := true
-LOCAL_STATIC_LIBRARIES := libcutils libc liblog libutils
-#LOCAL_SHARED_LIBRARIES := libcutils libc liblog libutils
-LOCAL_C_INCLUDES += $(JNI_H_INCLUDE):
+
+LOCAL_CFLAGS := -D_ANDROID_
+#LOCAL_CFLAGS += -std=c99 -DUSE_THREADS -D__ANDROID__
+#LOCAL_FORCE_STATIC_EXECUTABLE := true
+LOCAL_CFLAGS += -std=c++1y
+
+#LOCAL_SHARED_LIBRARIES := \
+	libbinder \
+	libcutils \
+	liblog \
+	libutils \
+	libpowermanager
+
+#LOCAL_STATIC_LIBRARIES := libcutils liblog libutils 
+#LOCAL_C_INCLUDES += $(JNI_H_INCLUDE):
+LOCAL_PROPRIETARY_MODULE := true
+#LOCAL_MODULE_OWNER := qcom
+#include $(BUILD_SHARED_LIBRARY)
+#####################################
+#include $(CLEAR_VARS)
+
+LOCAL_MODULE := suspend_service
+LOCAL_SRC_FILES := AWakeLock.cpp suspend_service.cpp
+LOCAL_SHARED_LIBRARIES := \
+	libbinder \
+	libhardware \
+	libhardware_legacy \
+	libcutils \
+	liblog \
+	libutils \
+	libpowermanager
+
 include $(BUILD_EXECUTABLE)

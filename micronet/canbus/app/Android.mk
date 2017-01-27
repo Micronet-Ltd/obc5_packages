d9f8e46e3736c5b6a224eea5266622335de843b5
Brigham Diaz <brigham.diaz@micronet-inc.com>
2017-01-27 15:30:19 -0700

    using external ndk-build

--- micronet/canbus/app/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH:= $(call my-dir)
-include $(CLEAR_VARS)
-JNI_SRC_PATH := $(LOCAL_PATH)/src/main/jni
-
-LOCAL_MODULE := canbus
-LOCAL_SRC_FILES := $(JNI_SRC_PATH)/FlexCANCanbusInterfaceBridge.cpp \
-                   $(JNI_SRC_PATH)/canbus.cpp
-include $(BUILD_SHARED_LIBRARY)
-

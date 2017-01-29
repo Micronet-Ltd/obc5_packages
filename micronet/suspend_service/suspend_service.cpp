368b522be6f4d5fadba5fba3bd5f33c7d58dbe27
igorl <igor.lantsman@micronet-inc.com>
2017-01-29 10:10:48 +0200

    Added shutdown timeout support.

--- micronet/suspend_service/suspend_service.cpp
+++ micronet/suspend_service/suspend_service.cpp
@@ -35,13 +35,15 @@ const char* ign_fname =     "/dev/ignition_level";
 const char* db_fname =      "/data/data/com.android.providers.settings/databases/settings.db";
 //const char* tmp_fname = "/sys/devices/suspend_timeout/suspend_timeout_tmp";
 const char* tmp_fname =     "/data/local/tmp/suspend_timeout_tmp";
-const char* i_tm_tmp_fname = "/data/local/tmp/ignition_timeout_tmp";
+const char* shd_tmp_fname = "/data/local/tmp/shutdown_timeout_tmp";
 //const char* val_fname = "/sys/devices/suspend_timeout/suspend_timeout";
 
 const char* db_put =    "settings put system screen_off_timeout";
 const char* db_get =    "settings get system screen_off_timeout";
 const char* db_put_st = "settings put system sleep_timeout";
 const char* db_get_st = "settings get system sleep_timeout";
+const char* db_put_shd = "settings put system shutdown_timeout";
+const char* db_get_shd = "settings get system shutdown_timeout";
 //ignition
 //echo 692 > sys/class/gpio/export
 //cat sys/class/gpio/gpio692/value
@@ -49,7 +51,7 @@ const char* db_get_st = "settings get system sleep_timeout";
 
 struct suspend_tm_thcontext {
     int     fd_sp_tm;
-    int     fd_ign_tm;
+    int     fd_shd_tm;
     int     fd_ign_lvl;
     int     last_ign_lvl;
     int     wl_flags;
@@ -103,7 +105,7 @@ void * db_proc(void * cntx)
 {
     struct suspend_tm_thcontext* ptc = (struct suspend_tm_thcontext*)cntx;
     int fd_db, wd, length, i, found = 0;
-    uint32_t last_val = 0, tmp_val = 0;
+    uint32_t last_val_sp = 0, last_val_shd = 0, tmp_val = 0;
     char cmd_buf[256];
 
     DINFO("db_proc starts");
@@ -126,17 +128,32 @@ void * db_proc(void * cntx)
         if(found) 
         {
             memset(cmd_buf, sizeof(cmd_buf), 0);
+            length = get_settings(cmd_buf, db_get_shd, shd_tmp_fname);
+            if(0 < length) 
+            {
+                tmp_val = strtol(cmd_buf, 0, 10);
+                DINFO("tmp_val %d; last_val_shd %d", tmp_val, last_val_shd);
+                if(0 != tmp_val && tmp_val != last_val_shd) 
+                {
+                    length = write(ptc->fd_shd_tm, cmd_buf, length); 
+                    if (0 < length) 
+                    {
+                        last_val_shd = tmp_val; 
+                    }
+                }
+            }
+            memset(cmd_buf, sizeof(cmd_buf), 0);
             length = get_settings(cmd_buf, db_get, tmp_fname);
             if(0 < length) 
             {
                 tmp_val = strtol(cmd_buf, 0, 10);
-                DINFO("tmp_val %d; last_val %d", tmp_val, last_val);
-                if(0 != tmp_val && tmp_val != last_val) 
+                DINFO("tmp_val %d; last_val_sp %d", tmp_val, last_val_sp);
+                if(0 != tmp_val && tmp_val != last_val_sp) 
                 {
                     length = write(ptc->fd_sp_tm, cmd_buf, length); 
                     if (0 < length) 
                     {
-                        last_val = tmp_val; 
+                        last_val_sp = tmp_val; 
                     }
                 }
             }
@@ -233,8 +250,8 @@ int main(int argc __attribute__((unused)), char * argv[] __attribute__((unused))
         return -1;
     }
 
-    tc.fd_ign_tm = open(ign_tm_fname, O_RDWR, O_NDELAY);
-    if(0 > tc.fd_ign_tm)
+    tc.fd_shd_tm = open(ign_tm_fname, O_RDWR, O_NDELAY);
+    if(0 > tc.fd_shd_tm)
     {
         DERR ("error open device %s", ign_fname);
         return -1;
@@ -259,8 +276,8 @@ int main(int argc __attribute__((unused)), char * argv[] __attribute__((unused))
 
     if(max_fd < tc.fd_sp_tm)
         max_fd = tc.fd_sp_tm;
-    if(max_fd < tc.fd_ign_tm)
-        max_fd = tc.fd_ign_tm;
+    if(max_fd < tc.fd_shd_tm)
+        max_fd = tc.fd_shd_tm;
     if(max_fd < tc.fd_ign_lvl)
         max_fd = tc.fd_ign_lvl;
 
@@ -270,7 +287,7 @@ int main(int argc __attribute__((unused)), char * argv[] __attribute__((unused))
         timeout.tv_usec = 0;
         FD_ZERO (&set);
         FD_SET (tc.fd_sp_tm, &set);
-        FD_SET (tc.fd_ign_tm, &set);
+        FD_SET (tc.fd_shd_tm, &set);
         FD_SET (tc.fd_ign_lvl, &set);
 
         ret = select(max_fd + 1, &set, NULL, NULL, &timeout);
@@ -288,21 +305,21 @@ int main(int argc __attribute__((unused)), char * argv[] __attribute__((unused))
                 {
                     sprintf(outbuffer, "%s %s\n", db_put, readbuffer); 
                     system(outbuffer);
+                    DINFO("%s", outbuffer);
                 }
-
-                DINFO("%s", outbuffer);
             }
         }
-        if(FD_ISSET(tc.fd_ign_tm, &set)) 
+        if(FD_ISSET(tc.fd_shd_tm, &set)) 
         {
-            len = read(tc.fd_ign_tm, readbuffer, sizeof(readbuffer)/sizeof(readbuffer[0]));
+            len = read(tc.fd_shd_tm, readbuffer, sizeof(readbuffer)/sizeof(readbuffer[0]));
             if(len > 0) 
             {
-                //if(0 != test_timeout(readbuffer)) 
-                //{
-                //}
-
-                DINFO("%s", readbuffer);
+                if(0 != test_timeout(readbuffer)) 
+                {
+                    sprintf(outbuffer, "%s %s\n", db_put_shd, readbuffer); 
+                    system(outbuffer);
+                    DINFO("%s", outbuffer);
+                }
             }
         }
         if(FD_ISSET(tc.fd_ign_lvl, &set)) 

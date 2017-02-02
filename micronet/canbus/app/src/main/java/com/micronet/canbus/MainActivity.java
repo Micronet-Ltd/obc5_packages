d045a48781202ebf2521f194bc5fc8ca93cb6047
Brigham Diaz <brigham.diaz@micronet-inc.com>
2017-02-01 22:13:18 -0500

    attempt to fix app permissions when opening tty port

--- micronet/canbus/app/src/main/java/com/micronet/canbus/MainActivity.java
+++ micronet/canbus/app/src/main/java/com/micronet/canbus/MainActivity.java
@@ -12,6 +12,13 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
         FlexCANCanbusInterfaceBridge canbus = new FlexCANCanbusInterfaceBridge();
         canbus.create();
     }

ccd1b04c6f39d3092d042a8a38002a67647d16d1
austin oneil <austin.oneil@micronet-inc.com>
2018-04-16 14:04:22 -0600

    Separated cradle only results from full test results

--- ehang/QrtFactoryKit/BatchScript/MT5_TEST_MAIN.bat
+++ ehang/QrtFactoryKit/BatchScript/MT5_TEST_MAIN.bat
@@ -9,7 +9,6 @@ set scriptversion=2.0.0
 del /F /Q temp.csv 1>nul 2>nul
 del /F /Q temp2.csv 1>nul 2>nul
 if not exist Results mkdir Results
-if not exist Results\summary.csv copy Input\template.csv Results\summary.csv >nul
 echo ************************************
 echo %messageIntro% %scriptversion%
 echo ************************************
@@ -37,13 +36,14 @@ if %%c == %param3% (set param3=Pass) else (set param3=Fail) || set param3=Fail
 if %tempfile:~0,1% EQU 3 (
     goto :cradleonly
 )
-for /F "tokens=1,2,3* delims=," %%a in ("%tempfile:~2%") do echo %DATE%,%TIME%,%%a,%param1%,%%b,%param2%,%%c,%param3%,%param4%,%%d,%scriptversion% >temp2.csv
+if not exist Results\full_test_summary.csv copy Input\full_test_template.csv Results\full_test_summary.csv >nul
+for /F "tokens=1,2,3* delims=," %%a in ("%tempfile:~2%") do echo %DATE%,%TIME%,%%a,%param1%,%%b,%param2%,%%c,%param3%,%%d,%scriptversion% >temp2.csv
 findstr Fail temp2.csv 1>nul 2>nul
 if %ERRORLEVEL%==0 @(
-  for /F "tokens=* delims=," %%a in (temp2.csv) do echo %%a,Fail >>Results\summary.csv
+  for /F "tokens=* delims=," %%a in (temp2.csv) do echo %%a,Fail >>Results\full_test_summary.csv
   color 47
 ) else (
-  for /F "tokens=* delims=," %%a in (temp2.csv) do echo %%a,Pass >>Results\summary.csv
+  for /F "tokens=* delims=," %%a in (temp2.csv) do echo %%a,Pass >>Results\full_test_summary.csv
   color 27
 )
 del /F /Q temp.csv 1>nul 2>nul
@@ -56,15 +56,16 @@ echo %messageTestNotFound%
 pause
 goto :eof
 :cradleonly
+if not exist Results\cradle_only_summary.csv copy Input\cradle_only_template.csv Results\cradle_only_summary.csv >nul
 for /F "tokens=1,2,3,4,5,6,7,8 delims=," %%a in ("%tempfile:~2%") do (
-  echo %DATE%,%TIME%,N/A,Not Tested,%%b,%param2%,%%c,N/A,%param4%,%%d,Not Tested,Not Tested,Not Tested,Not Tested,Not Tested,Not Tested,Not Tested,Not Tested,Not Tested,Not Tested,Not Tested,Not Tested,Not Tested,Not Tested,Not Tested,Not Tested,Not Tested,Not Tested,Not Tested,Not Tested,Not Tested,Not Tested,Not Tested,%%e,Not Tested,Not Tested,%%f,%%g,%%h,%scriptversion% >temp2.csv
+  echo %DATE%,%TIME%,%%b,%param2%,%%c,%param4%,%%d,%%e,%%f,%%g,%%h,%scriptversion% >temp2.csv
 )
 findstr Fail temp2.csv 1>nul 2>nul
 if %ERRORLEVEL%==0 @(
-for /F "tokens=* delims=," %%a in (temp2.csv) do echo %%a,Fail >>Results\summary.csv
+for /F "tokens=* delims=," %%a in (temp2.csv) do echo %%a,Fail >>Results\cradle_only_summary.csv
 color 47
 ) else (
-for /F "tokens=* delims=," %%a in (temp2.csv) do echo %%a,Pass >>Results\summary.csv
+for /F "tokens=* delims=," %%a in (temp2.csv) do echo %%a,Pass >>Results\cradle_only_summary.csv
 color 27
 )
 del /F /Q temp.csv 1>nul 2>nul
